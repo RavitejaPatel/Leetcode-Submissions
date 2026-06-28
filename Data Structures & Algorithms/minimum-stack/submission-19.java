47277a3ce3142cb4488b9e078252044fe41d7623
Raviteja Poosala <43495746+RavitejaPatel@users.noreply.github.com>
2026-06-27 22:12:00 -0700

    Add: minimum-stack - submission-19

---
+++ Data Structures & Algorithms/minimum-stack/submission-19.java
@@ -0,0 +1,37 @@
+class MinStack {
+    
+    Stack<Integer> stack = null;
+    Stack<Integer> minStack = null;
+
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
+    
+    public void push(int val) {
+        stack.add(val);
+        if(minStack.isEmpty() || val<=minStack.peek())    
+            minStack.push(val);
+    }   
+    
+    public void pop() {
+        if(stack.isEmpty())
+            return;
+        int peekVal = stack.peek();
+        if( !minStack.isEmpty() && peekVal == minStack.peek())
+            minStack.pop();
+        stack.pop();
+    }
+    
+    public int top() {
+       if(stack.isEmpty())
+            return -1;
+        return stack.peek(); 
+    }
+    
+    public int getMin() {
+        if(minStack.isEmpty())
+            return -1;
+        return minStack.peek();
+    }
+}
